45d8812bd1a06d2525000e95b698f1bfb2b7fd96
Huang Degil <huang.degil@gmail.com>
2018-11-02 18:12:27 +0900

    Deleted misplaced files

--- app/src/main/java/com/example/topcredu/helloaar/SayHello.java
+++
@@ -1,5 +0,0 @@
-package com.example.topcredu.helloaar;
-
-public interface SayHello {
-  String sayHello(String name);
-}
--- app/src/main/java/com/example/topcredu/helloaar/SayHelloImpl.java
+++
@@ -1,7 +0,0 @@
-package com.example.topcredu.helloaar;
-
-public class SayHelloImpl implements SayHello {
-  public String sayHello(String name) {
-    return "Hello " + name;
-  }
-}
